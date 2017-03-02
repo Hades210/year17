7f780c8aa4cff83f3a0d935e39dc48735da2d94b
Hades210 <zhaomingliang609@sina.com>
2017-03-02 23:40:52 +0800

    Create priority_queueSwapCheck.cpp

---
+++ stage1/Cplusplus/Reference/Containers/priority_queue/priority_queueSwapCheck.cpp
@@ -0,0 +1,29 @@
+// file : priority_queueSwapCheck.cpp
+// void swap(priority_queue& x) noexcept
+
+#include <iostream>
+#include <queue>
+
+#include "../common/common-print.hpp"
+
+using namespace std;
+
+int main()
+{
+	priority_queue<int> foo, bar;
+
+	foo.push(15);foo.push(30);foo.push(10);
+
+	bar.push(101); bar.push(202);
+
+	foo.swap(bar);
+
+	cout<<"size of foo: "<<foo.size()<<"\n";
+	cout<<"size of bar: "<<bar.size()<<"\n";
+
+	printInfo("foo",foo);
+	printInfo("bar",bar);
+
+	return 0;
+
+}
